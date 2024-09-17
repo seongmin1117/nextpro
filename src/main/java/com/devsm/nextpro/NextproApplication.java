e4d7585f1eafdb3d210f343e1f58303aaa6b2634
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-09-17 19:20:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/devsm/nextpro/NextproApplication.java
@@ -0,0 +1,13 @@
+package com.devsm.nextpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsm/nextpro/NextproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsm.nextpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
